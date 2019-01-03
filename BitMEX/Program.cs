5098680d3557bf6a9c2b8f3a27f00325f3486050
Christopher Tso <christso@gmail.com>
2019-01-04 08:46:42 +1100

    initial commit

---
+++ BitMEX/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace BitMEX
+{
+    class Program
+    {
+        private static string bitmexKey = "YOUR-API-KEY";
+        private static string bitmexSecret = "YOUR-API-SECRET";
+
+        private static void Main(string[] args)
+        {
+            Program p = new Program();
+            p.Run(args);
+        }
+
+        private void Run(string[] args)
+        {
+            BitMEXApi bitmex = new BitMEXApi(bitmexKey, bitmexSecret);
+            // var orderBook = bitmex.GetOrderBook("XBTUSD", 3);
+            var orders = bitmex.GetOrders();
+            // var orders = bitmex.DeleteOrders();
+            Console.WriteLine(orders);
+        }
+    }
+}
